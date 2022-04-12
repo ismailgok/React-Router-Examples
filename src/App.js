985f40bba962e4ad4f7aacea9c228b867b45f38f
ismailgok <ismayilgok16@gmail.com>
2022-04-12 14:36:23 +0300

    Document Format

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React from "react"
-import {Routes,Route,Link} from "react-router-dom";
-import Home from "./components/Home"
-import User from "./components/User"
-import Users from "./components/Users"
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import User from "./components/User";
+import Users from "./components/Users";
 import Example from "./components/Examplecss";
-import style from "./App.module.css"
+import style from "./App.module.css";
 
 function App() {
-
   return (
     <div className={style.container}>
-      <div className={style.nav}>
-        
-      <li className={style.list}>
-      <Link to="/" className={style.link}>Home</Link>
-      </li>
-      <li className={style.list}>
-      <Link to="/Users" className={style.link}>Users</Link>
-      </li>
-      <li className={style.list}>
-        <Link to="/example" className={style.link}>ModuleCss Example</Link>
-      </li>
-      
-      </div>
-     <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/users" element={<Users />} />
-       <Route path="/user/:id" element={<User />} /> 
-       <Route path="/example" element={<Example />} /> 
-     </Routes> 
+      <ul className={style.nav}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Users">Users</Link>
+        </li>
+        <li>
+          <Link to="/example">ModuleCss Example</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/user/:id" element={<User />} />
+        <Route path="/example" element={<Example />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,35 +1,31 @@
-import styles from "./users.module.css"
-import {useEffect,useState} from 'react'
-import {Link} from "react-router-dom"
+import styles from "./users.module.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Users() {
-    const [isLoading, setIsLoading] = useState(true)
-    const [users,setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/users`)
-        .then(res => res.json())
-        .then(data => setUsers(data))
-        .finally(setIsLoading(false))
-    },[])
-
-
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .finally(setIsLoading(false));
+  }, []);
 
   return (
     <div>
-        <h1 className={styles.asd}>Users</h1>
-        {
-          isLoading && <div>Loading...</div>
-        }
-        <ul>
-          {
-              users.map((user) => (
-                <li key={user.id}><Link to={`/user/${user.id}`}>{user.name}</Link></li>
-              ))
-          }
-        </ul>
+      <h1 className={styles.asd}>Users</h1>
+      {isLoading && <div>Loading...</div>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/user/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Users
+export default Users;
